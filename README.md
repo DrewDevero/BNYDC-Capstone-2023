6fe2c62e352ae114a3659047ca8dcef2e16cead1
Alston Devero-Belfon <andwele@alstons-mbp.mynetworksettings.com>
2023-08-14 17:52:50 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# BNYDC Capstone Project 2023 - B. Stuyvesant Champagne Proposal Prototype
---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const openPopupBtn = document.getElementById("openPopup");
+    const closePopupBtn = document.getElementById("closePopup");
+    const champagnePopup = document.getElementById("champagnePopup");
+
+    openPopupBtn.addEventListener("click", function() {
+        champagnePopup.style.right = "0"; // Move the popup into view from the right
+    });
+
+    closePopupBtn.addEventListener("click", function() {
+        champagnePopup.style.right = "-100%"; // Move the popup off the screen to the right
+    });
+
+    document.addEventListener('mousemove', function(e) {
+        const mouseY = e.clientY;
+        const windowHeight = window.innerHeight;
+      
+        // Calculate the position to trigger the hover effect
+        const triggerPosition = 0.335 * windowHeight;
+      
+        if (mouseY >= triggerPosition) {
+            champagnePopup.style.right = "0";
+        } else {
+            champagnePopup.style.right = "-100%";
+        }
+      });
+
+});
