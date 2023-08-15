581c4fa0cc8d19418d1714f40dd4470079328edb
Alston Devero-Belfon <deverobelfon@gmail.com>
2023-08-14 22:29:26 -0400

    added curved text to pop-up

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const openPopupBtn = document.getElementById("openPopup");
     const closePopupBtn = document.getElementById("closePopup");
     const champagnePopup = document.getElementById("champagnePopup");
+    const text = document.getElementById('curved-text')
 
     openPopupBtn.addEventListener("click", function() {
         champagnePopup.style.right = "0"; // Move the popup into view from the right
@@ -24,5 +25,12 @@ document.addEventListener("DOMContentLoaded", function() {
             champagnePopup.style.right = "-100%";
         }
       });
+
+    const curveThis = new CircleType(text)
+        .radius(325);
+
+    window.addEventListener('resize', () => {
+        curveThis.radius(curveThis.element.offsetWidth / 2);
+    });
 
 });
